6ca06ee1c9804333721d50500495a6e33c57c2ca
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-02-06 18:39:42 +0900

    Add App Theme

---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final ThemeData themeData = new ThemeData(
+  brightness: Brightness.light,
+  primaryColor: Colors.lightGreen,
+  primaryColorBrightness: Brightness.dark,
+  accentColor: Colors.indigoAccent,
+  dividerColor: Colors.grey[300],
+  backgroundColor: Colors.grey[100],
+  secondaryHeaderColor: Colors.white,
+);
