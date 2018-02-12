449ab13649901733fde2ef9118d782fade6b40d1
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-02-12 23:33:53 +0900

    Remove Tutorial comments

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -4,7 +4,6 @@ import 'package:qiita_mitsuke_tatter/ui/screen/home_screen.dart';
 import 'package:qiita_mitsuke_tatter/theme.dart';
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
