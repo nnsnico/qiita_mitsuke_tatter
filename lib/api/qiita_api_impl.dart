7e602bd09f2a26ed4d13203471e929fe67232949
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-02-22 16:49:41 +0900

    Add QiitaApi and QiitaApiImpl

---
+++ lib/api/qiita_api_impl.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:qiita_mitsuke_tatter/api/qiita_api.dart';
+import 'package:qiita_mitsuke_tatter/model/tag.dart';
+import 'package:qiita_mitsuke_tatter/model/topic.dart';
+import 'package:qiita_mitsuke_tatter/model/user.dart';
+
+class QiitaApiImpl implements QiitaApi {
+
+  var topicList = new List<Topic>();
+  var userList = new List<User>();
+
+  @override
+  Future<List<Topic>> getTopics() async {
+    if (topicList.isEmpty) {
+      await requestAll();
+    }
+
+    return topicList;
+  }
+
+  @override
+  Future<List<User>> getUsers() async {
+    if (userList.isEmpty) {
+      await requestAll();
+    }
+
+    return userList;
+  }
+
+  requestAll() async {
+    var response = await http.read('http://qiita.com/api/v2/items');
+    var json = JSON.decode(response);
+
+    for (var value in json) {
+      var userJson = value['user'];
+
+      // User
+      User user = new User(
+        userJson['name'],
+        userJson['id'],
+        userJson['description'],
+        userJson['profile_image_url'],
+      );
+
+      // Tag
+      var tags = value['tags'];
+      var tagList = new List<Tag>();
+      for (var tag in tags) {
+        var t = new Tag(
+          tag['name'],
+        );
+        tagList.add(t);
+      }
+
+      // Topic
+      Topic topic = new Topic(
+          value['title'],
+          value['body'],
+          value['likes_count'],
+          tagList,
+          value['updated_at'],
+          user,
+      );
+
+      topicList.add(topic);
+      userList.add(user);
+    }
+  }
+}
